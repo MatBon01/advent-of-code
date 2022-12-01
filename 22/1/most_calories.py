905293e2511f2089fe2567c65dfad8e59dea5f11
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-01 21:22:47 +0000

    Add interface

--- 22/1/most_calories.py
+++ 22/1/most_calories.py
@@ -1,4 +1,5 @@
 from typing import List, TextIO
+import sys
 from functools import reduce
 from pathlib import Path
 
@@ -23,3 +24,16 @@ def create_list_from_input(path: Path = Path("input.txt")) -> List[List[int]]:
             else:
                 calories_carried_by_current_elf.append(int(line))
     return calories_carried
+
+
+def main():
+    path: Path = Path("input.txt")
+    if len(sys.argv) > 1:
+        path: Path = Path(sys.argv[1])
+    calories_carried: List[List[int]] = create_list_from_input(path)
+    most_calories: int = most_calories_in_list(calories_carried)
+    print(most_calories)
+
+
+if __name__ == "__main__":
+    main()
