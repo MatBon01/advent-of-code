7f62456485a44b96bc197470c7179c59c8d67e57
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-01 21:36:47 +0000

    Extend code to work for part 2

--- 22/1/most_calories.py
+++ 22/1/most_calories.py
@@ -3,15 +3,25 @@ import sys
 from functools import reduce
 from pathlib import Path
 
+ELVES_TO_CONSIDER_INDEX: int = 1
+DEFAULT_NUM_ELVES: int = 1
 
-def most_calories_in_list(calories_carried: List[List[int]]) -> int:
-    # Pre: len(calories_carried) > 0
+INPUT_FILE_INDEX: int = 2
+DEFAULT_INPUT_FILE: str = "input.txt"
+
+
+def most_calories_in_list(
+    calories_carried: List[List[int]], elves_to_consider: int = DEFAULT_NUM_ELVES
+) -> int:
+    # Pre: n > 0
+    # Pre: len(calories_carried) >= n
     total_calories: List[int] = list(map(sum, calories_carried))
-    most_calories: int = reduce((lambda x, y: x if x > y else y), total_calories)
-    return most_calories
+    total_calories.sort(reverse=True)
+    calories_considered: List[int] = total_calories[:elves_to_consider]
+    return sum(calories_considered)
 
 
-def create_list_from_input(path: Path = Path("input.txt")) -> List[List[int]]:
+def create_list_from_input(path: Path = Path(DEFAULT_INPUT_FILE)) -> List[List[int]]:
     calories_carried: List[List[int]] = []
     calories_carried_by_current_elf: List[int] = []
     f: TextIO
@@ -27,11 +37,14 @@ def create_list_from_input(path: Path = Path("input.txt")) -> List[List[int]]:
 
 
 def main():
-    path: Path = Path("input.txt")
-    if len(sys.argv) > 1:
-        path: Path = Path(sys.argv[1])
+    path: Path = Path(DEFAULT_INPUT_FILE)
+    elves_to_consider: int = DEFAULT_NUM_ELVES
+    if len(sys.argv) > ELVES_TO_CONSIDER_INDEX:
+        elves_to_consider = int(sys.argv[ELVES_TO_CONSIDER_INDEX])
+    if len(sys.argv) > INPUT_FILE_INDEX:
+        path = Path(sys.argv[INPUT_FILE_INDEX])
     calories_carried: List[List[int]] = create_list_from_input(path)
-    most_calories: int = most_calories_in_list(calories_carried)
+    most_calories: int = most_calories_in_list(calories_carried, elves_to_consider)
     print(most_calories)
 
 
