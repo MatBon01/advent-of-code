55a3c277f654264a6f1278c5d29721a04fe59bd1
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 20:13:43 +0100

    Rename function to show only can use 4 elements

--- 22/6/SignalDetector.hs
+++ 22/6/SignalDetector.hs
@@ -1,12 +1,12 @@
-getStartOfPacketMarker :: String -> Int
-getStartOfPacketMarker input = getStartOfPacketMarker' input 0 Nothing Nothing Nothing Nothing
+getStartOfPacketMarker4 :: String -> Int
+getStartOfPacketMarker4 input = getStartOfPacketMarker4' input 0 Nothing Nothing Nothing Nothing
   where
-    getStartOfPacketMarker' :: String -> Int -> Maybe Char -> Maybe Char -> Maybe Char -> Maybe Char -> Int
-    getStartOfPacketMarker' [] n _ _ _ _ = n
-    getStartOfPacketMarker' (c:str) n Nothing c1 c2 c3 = getStartOfPacketMarker' str (n + 1) c1 c2 c3 (Just c)
-    getStartOfPacketMarker' (c:str) n (Just c4) (Just c3) (Just c2) (Just c1)
+    getStartOfPacketMarker4' :: String -> Int -> Maybe Char -> Maybe Char -> Maybe Char -> Maybe Char -> Int
+    getStartOfPacketMarker4' [] n _ _ _ _ = n
+    getStartOfPacketMarker4' (c:str) n Nothing c1 c2 c3 = getStartOfPacketMarker4' str (n + 1) c1 c2 c3 (Just c)
+    getStartOfPacketMarker4' (c:str) n (Just c4) (Just c3) (Just c2) (Just c1)
       | allDifferent packetMarker = n
-      | otherwise                 = getStartOfPacketMarker' str (n + 1) (Just c3) (Just c2) (Just c1) (Just c)
+      | otherwise                 = getStartOfPacketMarker4' str (n + 1) (Just c3) (Just c2) (Just c1) (Just c)
       where
         packetMarker = [c4, c3, c2, c1]
         allDifferent [] = True
@@ -17,4 +17,4 @@ getStartOfPacketMarker input = getStartOfPacketMarker' input 0 Nothing Nothing N
 main =
   do
     input <- getLine
-    print (getStartOfPacketMarker input)
+    print (getStartOfPacketMarker4 input)
