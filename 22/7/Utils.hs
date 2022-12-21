ac7cfa9ce141601f95bfaca0c6550ebb9328723c
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 22:35:35 +0100

    Derive Show for new file system types

--- 22/7/Utils.hs
+++ 22/7/Utils.hs
@@ -2,4 +2,4 @@ module Utils where
 
 data FileEntry = DirectoryEntry String | FileEntry String Int deriving(Show)
 data Command = Cd FileEntry | Ls [FileEntry] deriving(Show)
-data File = Directory [(String, File)] | File Int
+data File = Directory [(String, File)] | File Int deriving(Show)
