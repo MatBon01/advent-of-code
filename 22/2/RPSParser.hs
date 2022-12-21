1e06670134601dc8552fa09392a5873c347bda13
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 05:38:21 +0100

    Make parser for part 2

---
+++ 22/2/RPSParser.hs
@@ -0,0 +1,31 @@
+module RPSParser where
+-- Still learning parsec library and improving Haskell --
+import Utils
+import Text.ParserCombinators.Parsec
+
+strategyGuide :: GenParser Char st [(Move, Outcome)]
+strategyGuide =
+  do 
+    result <- many gameStrategy
+    eof
+    return result
+
+gameStrategy :: GenParser Char st (Move, Outcome)
+gameStrategy =
+  do
+    move <- move
+    char ' '
+    outcome <- outcome
+    char '\n'
+    return (move, outcome)
+
+move :: GenParser Char st Move
+move =
+  (char 'A' >> return Rock) <|> (char 'B' >> return Paper) <|> (char 'C' >> return Scissors)
+
+outcome :: GenParser Char st Outcome
+outcome =
+  (char 'X' >> return Lose) <|> (char 'Y' >> return Draw) <|> (char 'Z' >> return Win)
+
+parseStrategy :: String -> Either ParseError [(Move, Outcome)]
+parseStrategy = parse strategyGuide "(unknown)"
