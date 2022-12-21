2ca86a69d6de29fd5243fa74b9a89d0c9736f810
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 17:27:12 +0100

    Identify overlapping pairs

--- 22/4/PairAnalyser.hs
+++ 22/4/PairAnalyser.hs
@@ -11,9 +11,17 @@ obsoletePair (left, right) = right `include` left || left `include` right
 countObsoletePairs :: [(Range, Range)] -> Int
 countObsoletePairs = length . filter obsoletePair
 
+overlap :: (Range, Range) -> Bool
+overlap ((l1, u1), (l2, u2)) = firstUpper >= secondLower
+  where
+    (firstUpper, secondLower) = if l1 < l2 then (u1, l2) else (u2, l1)
+
+countOverlappingPairs :: [(Range, Range)] -> Int
+countOverlappingPairs = length . filter overlap
+
 main = 
   do
     input <- getContents
     case parsePairs input of
       Left err -> do print err
-      Right pairRanges -> do print (countObsoletePairs pairRanges)
+      Right pairRanges -> do print (countOverlappingPairs pairRanges)
