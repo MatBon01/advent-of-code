50110aaad197e9221cc6a4eff30d2495bc82b22c
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 18:18:37 +0100

    Define instruction parser

--- 22/5/CrateParser.hs
+++ 22/5/CrateParser.hs
@@ -26,4 +26,16 @@ crateLevel = return []
 
 instructions :: GenParser Char st [Instruction]
 instructions = 
-  do return []
+  do
+    many instruction
+
+instruction :: GenParser Char st Instruction
+instruction = 
+  do
+    string "move "
+    numToMove <- read <$> many1 digit
+    string " from "
+    originCrateIndex <- (read <$> many1 digit) - 1
+    destinationCrateIndex <- (read <$> many1 digit) - 1
+    char '\n'
+    return (numToMove, originCrateIndex, destinationCrateIndex)
