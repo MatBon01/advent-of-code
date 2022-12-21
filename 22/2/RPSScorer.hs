2034d24ce3d50409e58a915bed9a0d6897779404
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 04:49:27 +0100

    Implement shapeScore

--- 22/2/RPSScorer.hs
+++ 22/2/RPSScorer.hs
@@ -1,8 +1,18 @@
 import RPSParser
 
+shapeScore :: Move -> Int
+shapeScore Rock = 1
+shapeScore Paper = 2
+shapeScore Scissors = 3
+
+calculateScore :: [(Move, Move)] -> Int
+calculateScore ((m1,m2) : otherRounds) =
+  shapeScore m2 + calculateScore otherRounds
+calculateScore [] = 0
+
 main = 
   do 
     input <- getContents
     case parseGame input of
         Left err -> do print err
-        Right result -> do print result
+        Right result -> do print (calculateScore result)
