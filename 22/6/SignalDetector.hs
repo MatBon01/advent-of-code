16370f691f5d93db3ecf6578ab76fa5765dbb9d5
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 20:28:16 +0100

    Generalise solution

--- 22/6/SignalDetector.hs
+++ 22/6/SignalDetector.hs
@@ -1,20 +1,38 @@
-getStartOfPacketMarker4 :: String -> Int
-getStartOfPacketMarker4 input = getStartOfPacketMarker4' input 0 Nothing Nothing Nothing Nothing
+allDifferent :: Eq a => [a] -> Bool
+allDifferent [] = True
+allDifferent (x : []) = True
+allDifferent (x : xs) = not (elem x xs) && allDifferent xs
+
+getStartOfPacketMarkerPosition4 :: String -> Int
+getStartOfPacketMarkerPosition4 input = getStartOfPacketMarkerPosition4' input 0 Nothing Nothing Nothing Nothing
   where
-    getStartOfPacketMarker4' :: String -> Int -> Maybe Char -> Maybe Char -> Maybe Char -> Maybe Char -> Int
-    getStartOfPacketMarker4' [] n _ _ _ _ = n
-    getStartOfPacketMarker4' (c:str) n Nothing c1 c2 c3 = getStartOfPacketMarker4' str (n + 1) c1 c2 c3 (Just c)
-    getStartOfPacketMarker4' (c:str) n (Just c4) (Just c3) (Just c2) (Just c1)
+    getStartOfPacketMarkerPosition4' :: String -> Int -> Maybe Char -> Maybe Char -> Maybe Char -> Maybe Char -> Int
+    getStartOfPacketMarkerPosition4' [] n _ _ _ _ = n
+    getStartOfPacketMarkerPosition4' (c:str) n Nothing c1 c2 c3 = getStartOfPacketMarkerPosition4' str (n + 1) c1 c2 c3 (Just c)
+    getStartOfPacketMarkerPosition4' (c:str) n (Just c4) (Just c3) (Just c2) (Just c1)
       | allDifferent packetMarker = n
-      | otherwise                 = getStartOfPacketMarker4' str (n + 1) (Just c3) (Just c2) (Just c1) (Just c)
+      | otherwise                 = getStartOfPacketMarkerPosition4' str (n + 1) (Just c3) (Just c2) (Just c1) (Just c)
       where
         packetMarker = [c4, c3, c2, c1]
-        allDifferent [] = True
-        allDifferent (x:[]) = True
-        allDifferent (x:xs) = not (elem x xs) && allDifferent xs
+
+-- Second int says how long the marker should be --
+getStartOfPacketMarkerPosition :: Int -> String -> Int
+getStartOfPacketMarkerPosition n input = getStartOfPacketMarkerPosition' input 0 []
+  where
+    -- Takes the remaining string, current position and marker candidate
+    getStartOfPacketMarkerPosition' :: String -> Int -> [Char] -> Int
+    getStartOfPacketMarkerPosition' [] currPos _ = currPos
+    getStartOfPacketMarkerPosition' (c:str) currPos marker
+      | allDifferent marker && length marker == n = currPos
+      | otherwise                                 = getStartOfPacketMarkerPosition' str (currPos + 1) newMarker
+      where
+
+        newMarker = c : take (n - 1) marker
+
+
 
 
 main =
   do
     input <- getLine
-    print (getStartOfPacketMarker4 input)
+    print (getStartOfPacketMarkerPosition 14 input)
