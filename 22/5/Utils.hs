2beceac2dcfb847b01fd93bc6f2b5474242e4271
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 17:42:53 +0100

    Define an instruction

--- 22/5/Utils.hs
+++ 22/5/Utils.hs
@@ -1,2 +1,7 @@
 module Utils where
 type Crate = Char
+
+-- This tells the program to move how many crates from which stack to which stack
+-- (Number, OriginStack, DestinationStack)
+-- Stacks are numbered starting from 1
+type Instruction = (Int, Int, Int)
