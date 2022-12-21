c74f819621da88b7b3d1d87fbc36cebd5b924ab4
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 16:14:13 +0100

    Add priority calculator

--- 22/3/BagContentsAnalyser.hs
+++ 22/3/BagContentsAnalyser.hs
@@ -1,11 +1,15 @@
 import BagContentsParser
 import Utils
+import Data.Char
 
 getDuplicateItem :: ([Item], [Item]) -> Item
 getDuplicateItem (c1, c2) = head (filter (`elem` c1) c2)
 
 getPriority :: Item -> Int
-getPriority _ = 0
+getPriority c
+  | isLower c  = ord c - ord 'a' + 1
+  | isUpper c  = ord c - ord 'A' + (ord 'z' - ord 'a' + 1) + 1
+  | otherwise  = 0
 
 duplicateItemPrioritySum :: [([Item], [Item])] -> Int
 duplicateItemPrioritySum = sum . map (getPriority . getDuplicateItem)
