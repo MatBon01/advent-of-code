442a5ad074dc868bfef43ae0d6e7871cb4287a81
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 23:35:06 +0100

    Create Monkey calculator

--- 22/21/MonkeyCalculator.hs
+++ 22/21/MonkeyCalculator.hs
@@ -1,9 +1,23 @@
 import Utils
 import MonkeyParser
+import Data.Maybe
+
+calculateMonkey :: [(String, Monkey)] -> Monkey -> Int
+calculateMonkey monkeys (Val num) = num
+calculateMonkey monkeys (Add m1 m2) = calculateMonkeyOp monkeys m1 m2 (+)
+calculateMonkey monkeys (Sub m1 m2) = calculateMonkeyOp monkeys m1 m2 (-)
+calculateMonkey monkeys (Mult m1 m2) = calculateMonkeyOp monkeys m1 m2 (*)
+calculateMonkey monkeys (Div m1 m2) = calculateMonkeyOp monkeys m1 m2 Prelude.div
+
+calculateMonkeyOp :: [(String, Monkey)] -> String -> String -> (Int -> Int -> Int) -> Int
+calculateMonkeyOp monkeys m1 m2 op = calculateMonkey monkeys m1' `op` calculateMonkey monkeys m2'
+  where
+    m1' = fromJust (lookup m1 monkeys)
+    m2' = fromJust (lookup m2 monkeys)
 
 main =
   do
     input <- getContents
     case parseMonkeys input of
       Left err -> do print err
-      Right result -> do print result
+      Right monkeys -> do print (calculateMonkey monkeys (fromJust (lookup "root" monkeys)))
