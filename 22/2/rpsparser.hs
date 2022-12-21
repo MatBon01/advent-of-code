f3d23920508f3f17ce8e81a964f3f5df6700f5b1
Matteo Bongiovanni <mgb58@hotmail.co.uk>
2022-12-21 04:21:37 +0100

    Parse input

---
+++ 22/2/rpsparser.hs
@@ -0,0 +1,45 @@
+-- Not finished researching parsec library but using it to practice fundamentals --
+import Text.ParserCombinators.Parsec
+import GHC.IO.BufferedIO (readBufNonBlocking)
+
+data Move = Rock | Paper | Scissors deriving(Show) -- TODO:: check this derivation --
+
+strategyGuide :: GenParser Char st [(Move, Move)]
+strategyGuide = 
+  do result <- many gameRound
+     eof
+     return result
+
+gameRound :: GenParser Char st (Move, Move)
+gameRound =
+  do move1 <- move
+     char ' '
+     move2 <- move
+     char '\n'
+     return (move1, move2)
+
+move :: GenParser Char st Move
+move = 
+  rock <|> paper <|> scissors
+
+rock :: GenParser Char st Move
+rock = 
+  (char 'A' <|> char 'X') >> return Rock
+
+paper :: GenParser Char st Move
+paper = 
+  (char 'B' <|> char 'Y') >> return Paper
+
+scissors :: GenParser Char st Move
+scissors = 
+  (char 'C' <|> char 'Z') >> return Scissors
+  
+parseGame :: String -> Either ParseError [(Move, Move)]
+parseGame input = parse strategyGuide "(unknown)" input
+
+main = 
+  do 
+    input <- getContents
+    case parse strategyGuide "(stdin)" input of
+        Left err -> do print err
+        Right result -> do print result
