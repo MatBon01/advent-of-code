72199ae67f15d4c0d46b9f4f6179776f4b942953
Matteo Bongiovanni <40599507+MatBon01@users.noreply.github.com>
2023-12-07 16:44:31 +0000

    Complete part 2

--- 23/2/cube_conundrum.py
+++ 23/2/cube_conundrum.py
@@ -2,10 +2,9 @@ from enum import Enum, auto
 
 def main() -> None:
     INPUT_FILE = "input.txt"
-    BAG_CONTENTS = {Colour.RED: 12, Colour.GREEN: 13, Colour.BLUE: 14}
     games: list[list[dict[Colour, int]]] = parse_input(INPUT_FILE)
-    valid_games: list[int] = calculate_valid_games(games, BAG_CONTENTS)
-    print(sum(valid_games))
+    part1(games)
+    part2(games)
 
 class Colour(Enum):
     RED = auto()
@@ -19,6 +18,35 @@ def parse_input(input_file_name: str) -> list[list[dict[Colour, int]]]:
     with open(input_file_name, "r") as f:
         for line in f:
             res.append(parse_line(line[:-1])) # removes \n from line
+    return res
+
+def part1(games: list[list[dict[Colour, int]]]) -> None:
+    BAG_CONTENTS = {Colour.RED: 12, Colour.GREEN: 13, Colour.BLUE: 14}
+    valid_games: list[int] = calculate_valid_games(games, BAG_CONTENTS)
+    print(sum(valid_games))
+
+def part2(games: list[list[dict[Colour, int]]]) -> None:
+    minimum_cubes: list[dict[Colour, int]] = calculate_minimum_cubes_per_game(games)
+    minimum_cube_powers: list[int] = calculate_cube_powers(minimum_cubes)
+    print(sum(minimum_cube_powers))
+
+def calculate_minimum_cubes_per_game(games: list[list[dict[Colour, int]]]) -> list[dict[Colour, int]]:
+    return list(map(calculate_minimum_cubes_of_game, games))
+
+def calculate_cube_powers(cube_bag: list[dict[Colour, int]]) -> list[int]:
+    return list(map(calculate_cube_power, cube_bag))
+
+def calculate_minimum_cubes_of_game(rounds: list[dict[Colour, int]]) -> dict[Colour, int]:
+    res: dict[Colour, int] = {colour: 0 for colour in Colour}
+    for round in rounds:
+        for colour in Colour:
+            res[colour] = max(res[colour], round.get(colour, 0))
+    return res
+
+def calculate_cube_power(cubes: dict[Colour, int]) -> int:
+    res: int = 1
+    for colour in Colour:
+        res *= cubes.get(colour, 0)
     return res
 
 def calculate_valid_games(games: list[list[dict[Colour, int]]], game_bag: dict[Colour, int]) -> list[int]:
